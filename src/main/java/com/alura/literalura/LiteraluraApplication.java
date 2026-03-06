ce557d70390417d638d67ad45828bbe69cac89f0
MarceloRodriguesOliveira <marcelo--rodrigues2@hotmail.com>
2026-03-06 13:28:21 -0300

    feat: created main boot function and operations display

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,13 +1,27 @@
 package com.alura.literalura;
 
+import com.alura.literalura.components.MainMenuComponent;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.io.IOException;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
+	private final MainMenuComponent main;
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiteraluraApplication.class, args);
+	public LiteraluraApplication(MainMenuComponent main){
+		this.main = main;
 	}
 
+	public static void main(String[] args) throws IOException, InterruptedException {
+		ApplicationContext context = SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		main.getMenu();
+	}
 }
