1c23d417ee3ae3baa51f94533d8da2a3a51c3744
MarceloRodriguesOliveira <marcelo--rodrigues2@hotmail.com>
2026-03-06 15:35:24 -0300

    chore: added new method for language query

--- src/main/java/com/alura/literalura/repository/BookRepository.java
+++ src/main/java/com/alura/literalura/repository/BookRepository.java
@@ -4,7 +4,10 @@ import com.alura.literalura.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByTitle(String title);
+    Optional<Book> findByTitle(String title);
+
+    List<Book> findByLanguage(String language);
 }
